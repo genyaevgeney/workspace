62fb16e36f3b480de4a4dce531348b0081136a93
Yevhenii <yev.hryshchenko@gmail.com>
2019-09-05 18:28:26 +0300

    some change

--- controllers/userController.js
+++
@@ -1,9 +0,0 @@
-exports.toDonate = (req, res) => {
-	res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-    res.end("To donate");
-};
-
-exports.getStatistic = (req, res) => {
-	res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-    res.end("Get information");
-};
